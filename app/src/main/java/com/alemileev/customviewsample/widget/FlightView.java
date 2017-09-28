d54054068bc34ac0b3717909c5049d9d9054969a
Alexey Mileev <alemileev@gmail.com>
2017-09-29 01:43:35 +0300

    Children instantiation and params loading

--- app/src/main/java/com/alemileev/customviewsample/widget/FlightView.java
+++ app/src/main/java/com/alemileev/customviewsample/widget/FlightView.java
@@ -1,15 +1,39 @@
 package com.alemileev.customviewsample.widget;
 import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Dimension;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.Px;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.widget.FrameLayout;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.alemileev.customviewsample.R;
 import com.alemileev.customviewsample.model.Flight;
 
-public final class FlightView extends FrameLayout {
+@SuppressWarnings("FieldCanBeLocal")
+public final class FlightView extends ViewGroup {
+
+    @ColorInt private int headerColor;
+    @ColorInt private int headerTextColor;
+    @ColorInt private int cardBackgroundColor;
+    @ColorInt private int primaryTextColor;
+    @ColorInt private int secondaryTextColor;
+    @ColorInt private int dividerColor;
+
+    @Px private int paddingLeft;
+    @Px private int paddingTop;
+    @Px private int paddingRight;
+    @Px private int paddingBottom;
+    @Px private int paddingInner;
+
+    @Dimension(unit = Dimension.SP) private float primaryTextSize;
+    @Dimension(unit = Dimension.SP) private float secondaryTextSize;
 
     private TextView numberTextView;
     private TextView departureDateTextView;
@@ -39,24 +63,92 @@ public final class FlightView extends FrameLayout {
     }
 
     private void init(@NonNull Context context) {
-        setClipChildren(false);
         setClipToPadding(false);
 
-        // Inflate xml to this ViewGroup
-        inflate(context, R.layout.view_flight, this);
+        // Prepare colors
+        headerColor = ContextCompat.getColor(context, R.color.primary);
+        headerTextColor = ContextCompat.getColor(context, R.color.white_text);
+        cardBackgroundColor = ContextCompat.getColor(context, R.color.white);
+        primaryTextColor = ContextCompat.getColor(context, R.color.primary_text);
+        secondaryTextColor = ContextCompat.getColor(context, R.color.secondary_text);
+        dividerColor = ContextCompat.getColor(context, R.color.divider);
 
-        // Find child views
-        numberTextView = findViewById(R.id.number_tv);
-        departureDateTextView = findViewById(R.id.departure_date_tv);
-        takeOffTimeTextView = findViewById(R.id.take_off_time_tv);
-        departureCodeTextView = findViewById(R.id.departure_code_tv);
-        departureCityTextView = findViewById(R.id.departure_city_tv);
-        departureAirportNameTextView = findViewById(R.id.departure_airport_name_tv);
-        landingTimeTextView = findViewById(R.id.landing_time_tv);
-        arrivalCodeTextView = findViewById(R.id.arrival_code_tv);
-        arrivalCityTextView = findViewById(R.id.arrival_city_tv);
-        arrivalAirportNameTextView = findViewById(R.id.arrival_airport_name_tv);
-        durationTextView = findViewById(R.id.duration_tv);
+        // Prepare paddings
+        paddingLeft = dpToPx(8);
+        paddingTop = dpToPx(8);
+        paddingRight = dpToPx(8);
+        paddingBottom = dpToPx(8);
+        paddingInner = dpToPx(8);
+
+        // Prepare text sizes
+        primaryTextSize = 24;
+        secondaryTextSize = 14;
+
+        // Instantiate, setup and attach child views
+        numberTextView = new TextView(context);
+        numberTextView.setTextSize(primaryTextSize);
+        numberTextView.setTextColor(headerTextColor);
+        addView(numberTextView);
+
+        departureDateTextView = new TextView(context);
+        departureDateTextView.setTextSize(primaryTextSize);
+        departureDateTextView.setTextColor(headerTextColor);
+        addView(departureDateTextView);
+
+        takeOffTimeTextView = new TextView(context);
+        takeOffTimeTextView.setTextSize(primaryTextSize);
+        takeOffTimeTextView.setTextColor(primaryTextColor);
+        addView(takeOffTimeTextView);
+
+        departureCodeTextView = new TextView(context);
+        departureCodeTextView.setTextSize(primaryTextSize);
+        departureCodeTextView.setTextColor(primaryTextColor);
+        addView(departureCodeTextView);
+
+        departureCityTextView = new TextView(context);
+        departureCityTextView.setTextSize(secondaryTextSize);
+        departureCityTextView.setTextColor(secondaryTextColor);
+        addView(departureCityTextView);
+
+        departureAirportNameTextView = new TextView(context);
+        departureAirportNameTextView.setTextSize(secondaryTextSize);
+        departureAirportNameTextView.setTextColor(secondaryTextColor);
+        addView(departureAirportNameTextView);
+
+        landingTimeTextView = new TextView(context);
+        landingTimeTextView.setTextSize(primaryTextSize);
+        landingTimeTextView.setTextColor(primaryTextColor);
+        addView(landingTimeTextView);
+
+        arrivalCodeTextView = new TextView(context);
+        arrivalCodeTextView.setTextSize(primaryTextSize);
+        arrivalCodeTextView.setTextColor(primaryTextColor);
+        addView(arrivalCodeTextView);
+
+        arrivalCityTextView = new TextView(context);
+        arrivalCityTextView.setTextSize(secondaryTextSize);
+        arrivalCityTextView.setTextColor(secondaryTextColor);
+        addView(arrivalCityTextView);
+
+        arrivalAirportNameTextView = new TextView(context);
+        arrivalAirportNameTextView.setTextSize(secondaryTextSize);
+        arrivalAirportNameTextView.setTextColor(secondaryTextColor);
+        addView(arrivalAirportNameTextView);
+
+        durationTextView = new TextView(context);
+        durationTextView.setTextSize(secondaryTextSize);
+        durationTextView.setTextColor(secondaryTextColor);
+        addView(durationTextView);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // TODO:
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        // TODO:
     }
 
     public void bindFlight(@NonNull Flight flight) {
@@ -71,6 +163,14 @@ public final class FlightView extends FrameLayout {
         arrivalCityTextView.setText(flight.arrivalAirport.city);
         arrivalAirportNameTextView.setText(flight.arrivalAirport.name);
         durationTextView.setText(flight.duration);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    @Px
+    private int dpToPx(@Dimension(unit = Dimension.DP) int dp) {
+        final Resources resources = getResources();
+        final DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics);
     }
 
 }
