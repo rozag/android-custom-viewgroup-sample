7b071b8ac185771870f8b85f74fb771bdee25856
Alexey Mileev <alemileev@gmail.com>
2017-09-28 22:11:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# android-custom-viewgroup-sample
+## TODOs
+* More details
+* Link to the video
+* Screenshot
+* Warning about some hacks and hardcoded stuff
---
+++ app/src/main/java/com/alemileev/customviewsample/App.java
@@ -0,0 +1,12 @@
+package com.alemileev.customviewsample;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
