8d1df6b279985f6eb56115c0c9ef3f00589e766d
Alexey Mileev <alemileev@gmail.com>
2017-09-29 00:49:51 +0300

    Add XML-based view implementation

--- app/src/main/java/com/alemileev/customviewsample/MainActivity.java
+++ app/src/main/java/com/alemileev/customviewsample/MainActivity.java
@@ -2,6 +2,8 @@ package com.alemileev.customviewsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alemileev.customviewsample.model.Airport;
 import com.alemileev.customviewsample.model.Flight;
@@ -22,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind flight to the simple XML implementation
+        final View xmlFlightView = findViewById(R.id.xml_flight_view);
+        final TextView numberTextView = xmlFlightView.findViewById(R.id.number_tv);
+        final TextView departureDateTextView = xmlFlightView.findViewById(R.id.departure_date_tv);
+        final TextView takeOffTimeTextView = xmlFlightView.findViewById(R.id.take_off_time_tv);
+        final TextView departureCodeTextView = xmlFlightView.findViewById(R.id.departure_code_tv);
+        final TextView departureCityTextView = xmlFlightView.findViewById(R.id.departure_city_tv);
+        final TextView departureAirportNameTextView = xmlFlightView.findViewById(R.id.departure_airport_name_tv);
+        final TextView landingTimeTextView = xmlFlightView.findViewById(R.id.landing_time_tv);
+        final TextView arrivalCodeTextView = xmlFlightView.findViewById(R.id.arrival_code_tv);
+        final TextView arrivalCityTextView = xmlFlightView.findViewById(R.id.arrival_city_tv);
+        final TextView arrivalAirportNameTextView = xmlFlightView.findViewById(R.id.arrival_airport_name_tv);
+        final TextView durationTextView = xmlFlightView.findViewById(R.id.duration_tv);
+        numberTextView.setText(FLIGHT.number);
+        departureDateTextView.setText(FLIGHT.departureDate);
+        takeOffTimeTextView.setText(FLIGHT.takeOffTime);
+        departureCodeTextView.setText(FLIGHT.departureAirport.code);
+        departureCityTextView.setText(FLIGHT.departureAirport.city);
+        departureAirportNameTextView.setText(FLIGHT.departureAirport.name);
+        landingTimeTextView.setText(FLIGHT.landingTime);
+        arrivalCodeTextView.setText(FLIGHT.arrivalAirport.code);
+        arrivalCityTextView.setText(FLIGHT.arrivalAirport.city);
+        arrivalAirportNameTextView.setText(FLIGHT.arrivalAirport.name);
+        durationTextView.setText(FLIGHT.duration);
     }
 
 }
